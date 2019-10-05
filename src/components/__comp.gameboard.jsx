779ab1449d582c84bd76957fb3eedd5f04be3a18
david lavieri <dvlavieri@gmail.com>
2019-10-05 09:54:29 -0700

    gameboard v1 done, todo: set up header, timer, game state

--- src/components/__comp.gameboard.jsx
+++ src/components/__comp.gameboard.jsx
@@ -4,83 +4,175 @@ import Square from './comp.square';
 export default class Game extends Component {
 
   state = {
-    row: 10,
-    col: 10,
+    rows: 10,
+    cols: 10,
     mines: 10,
     board: [],
   }
 
-  componentWillMount = () => {
-    let rows = this.state.row;
-    let cols = this.state.col;
-    let game = [];
-    let chance = this.state.mines/(rows*cols);
+// set-up functions
 
-    let mineCount = 0;
+  getSurrounding = (sq) => {
+    let area = [], row=Math.floor(sq/this.state.cols), col=sq%this.state.cols;
+    let x = this.state.cols;
 
-    while (game.length < rows) {
-      let row = [];
-      for (let i =0; i < cols; i++) {
-        if (mineCount < this.state.mines && Math.random() <= chance) {
-          row.push({row: game.length, col: i, val: 'x', hidden: true});
-          mineCount++;
-        } else row.push({row: game.length, col: i, val: null, hidden: true});
+    if (row > 0) {
+      area.push(sq-x);
+      if (col > 0) {
+        area.push(sq-x-1);
       }
-      game.push(row);
-      row = [];
+      if (col < x-1) {
+        area.push(sq-x+1);
+      }
     }
 
-    console.log(game);
+    if (col > 0) {
+      area.push(sq-1);
+    }
+
+    if (col < x-1) {
+      area.push(sq+1);
+    }
+
+    if (row < this.state.rows-1) {
+      area.push(sq+x);
+      if (col > 0) {
+        area.push(sq+x-1);
+      }
+      if (col < x-1) {
+        area.push(sq+x+1);
+      }
+    }
+
+    return area;
+  }
+
+  setup = () => {
+    // generate blank board
+    let size = this.state.rows * this.state.cols, game = [];
+
+    while (game.length < size-1) {
+      for (let i =0; i < size; i++) {
+        let row = Math.floor(i/this.state.cols);
+        let col = i%this.state.cols;
+        game.push({id: i, val: null, hidden: true, mine: false, flag: false, row: row, col: col});
+      }
+    }
+
+
+    // plant mines randomly
+    let count = this.state.mines;
+
+    while (count > 0) {
+      let rand = Math.floor(Math.random() * size);
+      if (game[rand].mine === false) {
+        game[rand].mine = true;
+        count--;}
+      }
+
+    // tally surrounding mines
+    for (let sq of game) {
+      if (sq.mine === true) {
+        let area = this.getSurrounding(sq.id);
+        for (let i = 0; i < area.length; i++) {
+          let id = area[i];
+
+          if (game[id].val === null) {
+            game[id].val = 1
+          } else game[id].val++;
+        }
+      }
+
+    }
+
+
     this.setState({
       board: game,
     })
   }
 
-
-  handleGameOver = () => {
-    console.log('game over, loser');
+  componentWillMount = () => {
+    this.setup();
   }
 
-  handleMove = (row, col) => {
+// handle moves
+  handleGameOver = () => {
+    console.log('game over, loser');
+    alert('game over, loser');
+  }
+
+  handleMove = (sq) => {
     let game = this.state.board;
-    let sq = game[row][col];
+    let current = game[sq];
+    // reveal clicked square
+    current.hidden = false;
 
-    sq.hidden = false;
-
-    if (sq.val === 'x') {
+    // handle if you have clicked a mine
+    if (current.mine === true) {
       this.handleGameOver();
-    } else if (sq.val === null) {
-      if (game[row-1][col].val === null && game[row-1][col].hidden === true) {
-        this.handleMove(row-1, col);
+    }
+    // handle if expand
+    else if (current.val === null) {
+      let area = this.getSurrounding(sq);
+      for (let i = 0; i < area.length; i++) {
+        let id = area[i];
+        if (game[id].mine === false && game[id].hidden === true) {
+        this.handleMove(id);
       }
     }
+  }
+
+  this.setState({
+    board: game
+  });
+}
+
+// handle click events
+
+
+  handleClick = (e) => {
+    let i = parseInt(e.target.id);
+
+    setTimeout(() => {
+      if (!this.state.board[i].flag) {
+        this.handleMove(i);
+      }
+    }, 200);
+
+  }
+
+  handleDoubleClick = (e) => {
+
+    let i = parseInt(e.target.id);
+    let game = this.state.board;
+
+    game[i].flag = !game[i].flag;
 
     this.setState({
       board: game
-    });
+    })
   }
 
-  handleClick = (e) => {
-    let i = e.target.id.split(',');
-    let row = parseInt(i[0]);
-    let col = parseInt(i[1]);
-    this.handleMove(row , col);
-}
+// display function for Game
 
   makeBoard = (arr) => {
+    let board = [], row = [];
+    for (let i = 0; i < arr.length; i++) {
+      row.push(<Square
+          val={arr[i].val}
+          hidden={arr[i].hidden}
+          mine={arr[i].mine}
+          flag={arr[i].flag}
+          id={arr[i].id}
+          onClick={this.handleClick}
+          onDoubleClick={this.handleDoubleClick}/>);
 
-    const game = arr.map(arr => {
-      return <div className='row'> {arr.map(sq => {
-        return <Square
-          val={sq.val}
-          id={sq.row.toString() + ',' + sq.col.toString()}
-          hidden={sq.hidden}
-          onClick={this.handleClick} />
-      })} </div>
-    });
-
-    return game;
-
+      if ((i+1)%10 === 0) {
+        board.push(<div className='row'>{row}</div>);
+        row = [];
+    }
+  }
+    return board;
   }
 
 
--- src/components/comp.square.jsx
+++ src/components/comp.square.jsx
@@ -7,11 +7,13 @@ export default function Square (props) {
 
 
   return (
-    <div className='col square null' onClick={props.onClick}>
-      {props.hidden ? <div className='hidden'  id={props.id}>
+    <div className='col square null' onClick={props.onClick} onDoubleClick={props.onDoubleClick} id={props.id}>
+      {props.hidden ?
+      <div className='hidden'  id={props.id}>
+        {props.flag ? <i className={flag} /> : null}
       </div> :
       <div id={props.id}>
-      {props.val === 'x' ? <i className={bomb}/> : <span>{props.val}</span>}
+      {props.mine === true ? <i className={bomb}/> : <span>{props.val}</span>}
       </div>}
     </div>
   )
